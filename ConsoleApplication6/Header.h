075b75e7b7d7c92ee2c298c970b6f445ef8186a7
Minh Quan To <minhquan.to@unob.cz>
2023-01-27 17:45:59 +0100

    Add project files.

---
+++ ConsoleApplication6/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <list>
+
+using namespace sf;
+
+const int screenWidth = 1200;
+const int screenHeight = 800;
+
---
+++ ConsoleApplication6/Player.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Header.h"
+#include "Entity.h"
+
+class player : public Entity
+{
+public:
+    bool thrust;
+
+    player();
+
+    void update();
+
+};
